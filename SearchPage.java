271b9d47dea149e9fef8096577c9a1207acd6b86
hari-visu <harivisurajasekar@gmail.com>
2025-03-29 12:31:31 +0530

    Update SearchPage.java

--- SearchPage.java
+++ SearchPage.java
@@ -1,5 +1,21 @@
-package bestbuy;
+package com.guvi.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class SearchPage {
+    private WebDriver driver;
+    private By searchBox = By.id("gh-search-input");
 
+    public SearchPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void searchProduct(String productName) {
+        WebElement searchElement = driver.findElement(searchBox);
+        searchElement.sendKeys(productName);
+        searchElement.sendKeys(Keys.RETURN);
+    }
 }
