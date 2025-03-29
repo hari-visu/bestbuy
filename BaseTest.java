3fe9be9ecb3cf3515248ec25a9b558024ecbb770
hari-visu <harivisurajasekar@gmail.com>
2025-03-29 12:29:32 +0530

    Update BaseTest.java

--- BaseTest.java
+++ BaseTest.java
@@ -1,5 +1,20 @@
-package bestbuy;
+package com.guvi.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
+    protected static WebDriver driver;
 
+    public static void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/hariv/Downloads/chromedriver-win64/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    public static void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
