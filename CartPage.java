176fa4b680d5193665173953c86d283ca502ff81
hari-visu <harivisurajasekar@gmail.com>
2025-03-29 12:30:17 +0530

    Update CartPage.java

--- CartPage.java
+++ CartPage.java
@@ -1,5 +1,34 @@
-package bestbuy;
+package com.guvi.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.JavascriptExecutor;
 
 public class CartPage {
+    private WebDriver driver;
+    private JavascriptExecutor js;
+    
+    private By addToCartButton = By.xpath("//button[text()='Add to Cart']");
+    private By cartButton = By.xpath("//span[text()='Cart']");
+    private By checkoutButton = By.xpath("//button[text()='Checkout']");
+    private By continueAsGuestButton = By.xpath("//button[text()='Continue as Guest']");
 
+    public CartPage(WebDriver driver) {
+        this.driver = driver;
+        this.js = (JavascriptExecutor) driver;
+    }
+
+    public void addToCart() throws InterruptedException {
+        js.executeScript("window.scrollBy(0, 700)");
+        Thread.sleep(3000);
+        driver.findElement(addToCartButton).click();
+    }
+
+    public void proceedToCheckout() throws InterruptedException {
+        driver.findElement(cartButton).click();
+        Thread.sleep(3000);
+        driver.findElement(checkoutButton).click();
+        Thread.sleep(3000);
+        driver.findElement(continueAsGuestButton).click();
+    }
 }
