f59ce19d35680a16d9d47c2f02dfad485c8337ec
hari-visu <harivisurajasekar@gmail.com>
2025-03-29 12:30:45 +0530

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -1,5 +1,28 @@
-package bestbuy;
+package com.guvi.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class HomePage {
+    private WebDriver driver;
+    
+    private By countrySelection = By.xpath("//img[@alt='United States']");
+    private By accountMenu = By.id("account-menu-account-button");
+    
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
 
+    public void openBestBuy() {
+        driver.get("https://www.bestbuy.com/");
+    }
+
+    public void selectCountry() {
+        driver.findElement(countrySelection).click();
+    }
+
+    public void openAccountMenu() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.findElement(accountMenu).click();
+    }
 }
